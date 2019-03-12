7e38abcf6b7f1d59a28d07101144db3f8bf14d42
plhnk <paul.hanaoka@gmail.com>
2019-03-12 14:10:32 -0700

    abstract constants out to smaller files

---
+++ constants/index.js
@@ -0,0 +1,124 @@
+// @flow
+import * as d3 from "d3";
+import { tracks, trackIds } from "../constants/tracks";
+import { titles } from  "../constants/titles";
+
+export type TrackId =
+  | "RESEARCH"
+  | "ENGINEERING"
+  | "INTERACTION"
+  | "STRATEGY"
+  | "VISUAL"
+  | "ADVOCACY"
+  | "IMPACT"
+  | "COLLABORATION"
+  | "INFLUENCE"
+  | "EXCELLENCE";
+
+export type Track = {
+  displayName: string,
+  category: string, // TK categoryId type?
+  description: string,
+  milestones: {
+    summary: string,
+    signals: string[],
+    examples: string[]
+  }[]
+};
+
+export type Milestone = 0 | 1 | 2 | 3 | 4 | 5;
+
+export type MilestoneMap = {
+  RESEARCH: Milestone,
+  ENGINEERING: Milestone,
+  INTERACTION: Milestone,
+  STRATEGY: Milestone,
+  VISUAL: Milestone,
+  ADVOCACY: Milestone,
+  IMPACT: Milestone,
+  COLLABORATION: Milestone,
+  INFLUENCE: Milestone,
+  EXCELLENCE: Milestone
+};
+export const milestones = [0, 1, 2, 3, 4, 5];
+
+// each milestone level is worth this many points
+export const milestoneToPoints = (milestone: Milestone): number => {
+  switch (milestone) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 3;
+    case 3:
+      return 6;
+    case 4:
+      return 12;
+    case 5:
+      return 20;
+    default:
+      return 0;
+  }
+};
+
+// how many points it takes to reach a new level
+export const pointsToLevels = {
+  "0": "0",
+  "8": "1",
+  "16": "2",
+  "32": "3",
+  "56": "4",
+  "96": "5"
+};
+
+export const maxLevel = 135;
+
+export const categoryIds: Set<string> = trackIds.reduce((set, trackId) => {
+  set.add(tracks[trackId].category);
+  return set;
+}, new Set());
+
+export const categoryPointsFromMilestoneMap = (milestoneMap: MilestoneMap) => {
+  let pointsByCategory = new Map();
+  trackIds.forEach(trackId => {
+    const milestone = milestoneMap[trackId];
+    const categoryId = tracks[trackId].category;
+    let currentPoints = pointsByCategory.get(categoryId) || 0;
+    pointsByCategory.set(
+      categoryId,
+      currentPoints + milestoneToPoints(milestone)
+    );
+  });
+  return Array.from(categoryIds.values()).map(categoryId => {
+    const points = pointsByCategory.get(categoryId);
+    return { categoryId, points: pointsByCategory.get(categoryId) || 0 };
+  });
+};
+
+export const totalPointsFromMilestoneMap = (
+  milestoneMap: MilestoneMap
+): number =>
+  trackIds
+    .map(trackId => milestoneToPoints(milestoneMap[trackId]))
+    .reduce((sum, addend) => sum + addend, 0);
+
+export const categoryColorScale = d3
+  .scaleOrdinal()
+  .domain(categoryIds)
+  .range(["#0b5fff", "#7d8b94"]);
+
+export const eligibleTitles = (milestoneMap: MilestoneMap): string[] => {
+  const totalPoints = totalPointsFromMilestoneMap(milestoneMap);
+
+  return titles
+    .filter(
+      title =>
+        (title.minPoints === undefined || totalPoints >= title.minPoints) &&
+        (title.maxPoints === undefined || totalPoints <= title.maxPoints)
+    )
+    .map(title => title.label);
+};
+
+export { tracks, trackIds, titles }
+export type { Track, Trackid }
---
+++ constants/titles.js
@@ -0,0 +1,55 @@
+export const titles = [
+    { 
+        label: "Add points to see titles", 
+        minPoints: 0, 
+        maxPoints: 0 
+    },
+    { 
+        label: "Associate Product Designer", 
+        minPoints: 8, 
+        maxPoints: 15 
+    },
+    { 
+        label: "Intern", 
+        minPoints: 1, 
+        maxPoints: 7 
+    },
+    { 
+        label: "Product Designer", 
+        minPoints: 16, 
+        maxPoints: 31 
+    },
+    { 
+        label: "Senior Product Designer", 
+        minPoints: 32, 
+        maxPoints: 55 
+    },
+    { 
+        label: "Senior Product Designer, Manager", 
+        minPoints: 32, 
+        maxPoints: 55 
+    },
+    {
+        label: "Senior Product Designer, Manager [Region]",
+        minPoints: 32,
+        maxPoints: 55
+    },
+    { 
+        label: "Staff Product Designer", 
+        minPoints: 56, 
+        maxPoints: 95 
+    },
+    { 
+        label: "Staff Product Designer, Manager", 
+        minPoints: 56, 
+        maxPoints: 95 
+    },
+    {
+        label: "Staff Product Designer, Manager [Region]",
+        minPoints: 56,
+        maxPoints: 95
+    },
+    { 
+        label: "Principal Product Designer", 
+        minPoints: 96 }
+];
